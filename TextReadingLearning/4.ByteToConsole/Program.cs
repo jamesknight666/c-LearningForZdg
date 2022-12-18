7aa1e5c08b6db2125fd4d705c33278cf5dd02e57
JamesKing <1919419993@qq.com>
2022-12-18 23:12:14 +0800

    第一次整合1-5到一个文件

---
+++ TextReadingLearning/4.ByteToConsole/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.ByteToConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (FileStream fs = new FileStream(@"C:\Users\DELL\桌面\zdg学习\c#LearningForZdg\Test1.txt", FileMode.OpenOrCreate, FileAccess.Read))
+            {
+                byte[] by = new byte[1];
+                while (true)
+                {
+                    int r = fs.Read(by, 0, by.Length);
+                    //十六进制另一种写法
+                    //Console.WriteLine(BitConverter.ToString(by));
+                    foreach (byte b in by)
+                    {
+                        Console.WriteLine(Convert.ToString(b, 2));
+                    }
+                    if (r == 0)
+                        break;
+                }
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ TextReadingLearning/5.EveryTenBytesToConsoleNewLine/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5.EveryTenBytesToConsoleNewLine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (FileStream fs = new FileStream(@"C:\Users\DELL\桌面\zdg学习\c#LearningForZdg\Test1.txt", FileMode.OpenOrCreate, FileAccess.Read))
+            {
+                while (true)
+                {
+                    byte[] by = new byte[10];
+                    int r = fs.Read(by, 0, by.Length);
+                    if (r == 0)
+                        break;
+                    foreach (byte b in by)
+                    {
+                        string s = Convert.ToString(b, 2);
+                        if (s != "0")
+                        Console.Write("{0}  ", s);
+                    }
+                    Console.Write('\n');
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
